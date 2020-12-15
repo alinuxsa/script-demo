abffc82c3d9b69bb6c80064a516f421cb46dc047
脑袋总进水 <alinuxsa@gmail.com>
2020-12-15 10:14:34 +0800

    Update exam.js

--- exam.js
+++ exam.js
@@ -1,14 +1,10 @@
 function Notify()
 {
     console.log("开始发送通知");
-    var Title,SubTitle,Content;
-    TItle = "我是主标题";
-    SubTitle  = "我是子标题";
-    Content = "我是要推送的内容";
-    $notification.post(Title,SubTitle,Content);
+    $notification.post('Title','SubTitle','Content');
     
 }
-//Call Function
+
 Notify()
 
 console.log("结束发送通知");
